4f557eabdabce75269abf998c6cb39eacf6e7c21
Egor <megaegor321@gmail.com>
2025-08-27 01:01:06 +0500

    Initial commit

---
+++ src/main/java/com/testjavaapp/uzticket/UzticketApplication.java
@@ -0,0 +1,12 @@
+package com.testjavaapp.uzticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UzticketApplication.class, args);
+	}
+}
---
+++ src/test/java/com/testjavaapp/uzticket/UzticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testjavaapp.uzticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UzticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
